73ec29069df7e98a3b2bba5f3bb2ce369bc4370d
brianbryank <brianbryan0125@gmail.com>
2023-09-18 19:32:08 +0300

    passed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,35 @@
+
+# name = 'Victor'
+# names = ['John', 'Kevin', 'Hum', 'Joel','Bilnas']
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badge_msg = []
+    for n in names:
+        badge_msg.append(f'Hello, my name is {n}.')
+    return badge_msg
+        
 
 def assign_rooms(names):
-    return None
+    room_assignment = []
+    rooms = range(1, 7)
+
+    for i, n in enumerate(names):
+        if i < len(rooms):
+            room_num = rooms[i]
+            room_assignment.append(f'Hello, {n}! You\'ll be assigned to room {room_num}!')
+        
+    return room_assignment
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignment = assign_rooms(names)
+
+    for bagde in badges:
+        print(bagde)
+
+    for assignment in room_assignment:
+        print(assignment)
+   
